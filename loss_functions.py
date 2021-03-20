6b313a01e19fa0d4eb12c6d3eb68ad79b2270ed0
Omar Magdy <omagdy.222@gmail.com>
2021-03-20 23:41:03 +0100

    Minor fix: l2 loss function

--- loss_functions.py
+++ loss_functions.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 
+mse = tf.keras.losses.MeanSquaredError()
+
 def supervised_loss(real, fake):
-	# loss = tf.reduce_mean(tf.square(real - fake)) # L2 Loss
-	loss = tf.losses.mean_squared_error(real, fake) # L2 Loss
-    # loss = tf.reduce_mean(tf.abs(real - fake))  # L1 Loss
-	return loss
+#     loss = tf.reduce_mean(tf.square(real - fake)) # L2 Loss
+    loss = mse(real, fake) # L2 Loss
+#     loss = tf.reduce_mean(tf.abs(real - fake))  # L1 Loss
+    return loss
