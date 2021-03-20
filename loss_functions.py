293513fe49aaa0596014a1f169663df404430aa1
Omar Magdy <omagdy.222@gmail.com>
2021-03-20 19:17:05 +0100

    Utilize Xavier for weight initialization

--- loss_functions.py
+++ loss_functions.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 def supervised_loss(real, fake):
-	loss = tf.reduce_mean(tf.square(real - fake)) # L2 Loss
+	# loss = tf.reduce_mean(tf.square(real - fake)) # L2 Loss
+	loss = tf.losses.mean_squared_error(real, fake) # L2 Loss
     # loss = tf.reduce_mean(tf.abs(real - fake))  # L1 Loss
 	return loss
